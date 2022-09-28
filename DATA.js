5a806ebc6bb1b5fba51b99aee34a8bda1f966635
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 14:01:29 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "598dab1b-6a90-4200-8751-86df78d38af1",
+    "home_feed": [
+        {
+            "id": "843cc7fd-f64c-4e31-9d43-29f0b5fc08be",
+            "artist_name": "condition",
+            "home_carousel": [
+                {
+                    "id": "bcbf93fb-a0b5-471f-9c58-80992a840248",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "8fda2b6b-dcdb-4bac-97b2-9f9edf2a8f57",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "65990387-52b5-4f74-bbac-ea482c99d434",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/69.png"
+                },
+                {
+                    "id": "44673f69-b4e5-48b5-9e11-651f5c060003",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "311b389d-b0ff-4956-b0d3-b80bcdeb7793",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                }
+            ],
+            "art_caption": "of",
+            "art_description": "Suffer alone start decision see number. Argue science particularly must. Marriage pull magazine produce attack him."
+        },
+        {
+            "id": "03f2a86c-4b47-4193-abe0-bfa7dbbb4810",
+            "artist_name": "send",
+            "home_carousel": [
+                {
+                    "id": "7a4a7fd6-be29-4f38-9f0b-d3ca53a11127",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "17440252-d515-4acb-ad08-1a77f5689d44",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "99b77a49-63ed-47c6-aa82-59a1ac6b4b5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "8db8fb34-cd59-4420-9979-d4c638e78f0a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/278.png"
+                },
+                {
+                    "id": "574672cb-5f05-4a73-a8d9-046b70f2d6db",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                }
+            ],
+            "art_caption": "war",
+            "art_description": "War a important sometimes nearly sign. Technology according price to opportunity. Kind husband film usually. Commercial hope arrive town throw."
+        },
+        {
+            "id": "4a160515-e29c-4301-99e4-160c5b8da8f7",
+            "artist_name": "sister",
+            "home_carousel": [
+                {
+                    "id": "8a1b06f8-c90f-4175-90f4-58503b65512b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "bba6dd8b-6b9d-4a65-ae9f-929fa26d9d0c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png"
+                },
+                {
+                    "id": "025ef6a2-9ea0-4341-b647-d35ac57f8e3c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "b04989b9-92ec-40c5-abf0-3853a2bd64a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "1e4943be-ea26-45f9-8a97-ba44ea6afe5d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                }
+            ],
+            "art_caption": "little",
+            "art_description": "Pick similar sit. Just just short theory. Letter attack support opportunity year sort."
+        },
+        {
+            "id": "9bfd6a65-4e92-4fe4-ae64-c9f2c6dec4b0",
+            "artist_name": "floor",
+            "home_carousel": [
+                {
+                    "id": "42875231-ccd2-4f18-ae28-b043eb2adc83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                },
+                {
+                    "id": "45487226-44d6-412c-b170-243c9772413a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "07ae032f-f1d7-4df4-a152-ea8dd5983cb0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "3dfb0977-ef4d-4eeb-8e01-4aacdef3c6f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "7001f562-f8cb-46c7-b2bd-12ed22257bdf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                }
+            ],
+            "art_caption": "break",
+            "art_description": "Poor break ask summer arm anyone. Nice property politics manager painting grow every rest."
+        },
+        {
+            "id": "c1063473-fc66-4c85-b1bc-af4d86a432ba",
+            "artist_name": "space",
+            "home_carousel": [
+                {
+                    "id": "caf456f5-b803-4021-bdbc-3070fe2ca891",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "7ff8e046-dd46-43cd-92c2-4c41079b273f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "1ca128d7-6693-4c69-96bd-b66fb6feb2f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                },
+                {
+                    "id": "b8d7a420-8b44-46ce-84a6-6f6973279ce9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "69ed86f0-9f4a-406c-bb1a-b461a13c0668",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/152.png"
+                }
+            ],
+            "art_caption": "almost",
+            "art_description": "Position blue policy step on product himself. Face something produce cell five decide. Together so some edge yard respond."
+        }
+    ]
+}
